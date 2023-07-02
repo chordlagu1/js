2de009efd3e72f62c3d175f7779288ac6911136c
chordlagu1 <138396225+chordlagu1@users.noreply.github.com>
2023-07-03 01:43:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_NAME = "your-cache-name";
 const urlsToCache = [
   "https://blogger.googleusercontent.com/img/a/AVvXsEjbAa9Sif_LtljvGY6gLSp6YvaRkXugDw_02v-vtUMG0A6XHERU8nG62GIX2YOB6wSzU7EIzoVxZyGDra4Sopl1Nf-cQBu6qBnwRzgyGtFieDilxNoegCXjlqfdFdeE7X5mZYlgL2IeDEEory2mC3YqB7CHEG5fCMHhYRiWqbbeDw8rYCzIvIg43I0hIWI=s128",
   "https://blogger.googleusercontent.com/img/a/AVvXsEjbAa9Sif_LtljvGY6gLSp6YvaRkXugDw_02v-vtUMG0A6XHERU8nG62GIX2YOB6wSzU7EIzoVxZyGDra4Sopl1Nf-cQBu6qBnwRzgyGtFieDilxNoegCXjlqfdFdeE7X5mZYlgL2IeDEEory2mC3YqB7CHEG5fCMHhYRiWqbbeDw8rYCzIvIg43I0hIWI=s128",
-  "https://github.com/chordlagu1/js/blob/main/shop-online.js",
+  "https://cdn.statically.io/gh/chordlagu1/js/main/shop-online.js",
   "https://cdn.statically.io/gh/chordlagu1/js/main/shop-style.css",
   // Tambahkan daftar URL aset yang ingin Anda cache untuk offline access
 ];
